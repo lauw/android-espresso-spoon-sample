9eabccbf78699294c38549d383e0e50c0d81ccb2
Laurens <laurensmuller14@gmail.com>
2014-12-11 00:09:21 +0100

    fixes for lollipop
    -- use a diff gradle dependency with some fixes (https://github.com/freezy/double-espresso)
    -- disable taking screenshots

--- app/src/androidTest/java/com/muller/uitest/LoginTest.java
+++ app/src/androidTest/java/com/muller/uitest/LoginTest.java
@@ -15,6 +15,8 @@ import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMat
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+/** Taking screenshots is disabled due to a (bug?) in lollipop / spoon, where spoon does not have permission to grab screenshots of of the device
+ ** more info: https://github.com/square/spoon/issues/189 **/
 public class LoginTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	public LoginTest() {
 		super(MainActivity.class);
@@ -27,7 +29,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	}
 
 	public void testLogin_Success() {
-		Spoon.screenshot(getActivity(), "beforeLogin");
+		//Spoon.screenshot(getActivity(), "beforeLogin");
 
 		onView(withId(R.id.login_txtUsername)).perform(typeText("howdy"), closeSoftKeyboard());
 		sleep(300);
@@ -38,11 +40,11 @@ public class LoginTest extends ActivityInstrumentationTestCase2<MainActivity> {
 		onView(withId(R.id.login_btnSubmit)).perform(click());
 
 		onView(withId(R.id.login_txtResult)).check(matches(withText(R.string.login_successful)));
-		Spoon.screenshot(getActivity(), "afterLogin");
+		//Spoon.screenshot(getActivity(), "afterLogin");
 	}
 
 	public void testLogin_Error() {
-		Spoon.screenshot(getActivity(), "beforeLogin");
+		//Spoon.screenshot(getActivity(), "beforeLogin");
 
 		onView(withId(R.id.login_txtUsername)).perform(typeText("howdie"), closeSoftKeyboard());
 		sleep(300);
@@ -53,7 +55,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2<MainActivity> {
 		onView(withId(R.id.login_btnSubmit)).perform(click());
 
 		onView(withId(R.id.login_txtResult)).check(matches(withText(R.string.login_error)));
-		Spoon.screenshot(getActivity(), "afterLogin");
+		//Spoon.screenshot(getActivity(), "afterLogin");
 	}
 
 	/** Usually you will not have to use sleep, this is due to the action 'closeSoftKeyboard', which requires some time  **/
