b19f85873503fd7dfd5b1e5a4bcc3a9d25a556a6
Laurens <laurensmuller14@gmail.com>
2014-12-11 00:06:30 +0100

    Simple test

---
+++ app/src/androidTest/java/com/muller/uitest/LoginTest.java
@@ -0,0 +1,68 @@
+package com.muller.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.squareup.spoon.Spoon;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class LoginTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	public LoginTest() {
+		super(MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		getActivity();
+	}
+
+	public void testLogin_Success() {
+		Spoon.screenshot(getActivity(), "beforeLogin");
+
+		onView(withId(R.id.login_txtUsername)).perform(typeText("howdy"), closeSoftKeyboard());
+		sleep(300);
+
+		onView(withId(R.id.login_txtPassword)).perform(typeText("hello"), closeSoftKeyboard());
+		sleep(300);
+
+		onView(withId(R.id.login_btnSubmit)).perform(click());
+
+		onView(withId(R.id.login_txtResult)).check(matches(withText(R.string.login_successful)));
+		Spoon.screenshot(getActivity(), "afterLogin");
+	}
+
+	public void testLogin_Error() {
+		Spoon.screenshot(getActivity(), "beforeLogin");
+
+		onView(withId(R.id.login_txtUsername)).perform(typeText("howdie"), closeSoftKeyboard());
+		sleep(300);
+
+		onView(withId(R.id.login_txtPassword)).perform(typeText("hell0"), closeSoftKeyboard());
+		sleep(300);
+
+		onView(withId(R.id.login_btnSubmit)).perform(click());
+
+		onView(withId(R.id.login_txtResult)).check(matches(withText(R.string.login_error)));
+		Spoon.screenshot(getActivity(), "afterLogin");
+	}
+
+	/** Usually you will not have to use sleep, this is due to the action 'closeSoftKeyboard', which requires some time  **/
+	public static void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
